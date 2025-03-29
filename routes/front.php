daa901d6fba6bdb6f491e24b917a08f54574b2c3
I'm KB <tttrash27@gmail.com>
2025-03-29 21:13:09 +0530

    new routes file add

---
+++ routes/front.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\HomeController;
+
+Route::get('/', [HomeController::class, 'home'])->name('user.home');
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Front\HomeController;
+// use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 
-Route::get('/', [HomeController::class, 'home'])->name('user.home');
+// Route::get('/', [HomeController::class, 'home'])->name('user.home');
 Route::get('login', [AuthController::class, 'userLogin'])->name('user.login');
 Route::post('login-submit', [AuthController::class, 'userLoginSubmit'])->name('user.login.submit');
 
 Route::prefix('admin')->group(function () {
     Route::get('dashboard', [AdminDashboardController::class, 'dashboard'])->name('admin.dashboard');
 });
+
+include('front.php');
